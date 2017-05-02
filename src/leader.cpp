e177c835d96f9a0155941bcf261dcbac4ef7b356
Luke Wright <lukewright1997@gmail.com>
2017-05-01 21:32:06 -0500

    implement logic to add goal, will need to be cleaned up

--- src/leader.cpp
+++ src/leader.cpp
@@ -4,6 +4,7 @@
 #include <bwi_msgs/QuestionDialog.h>
 
 typedef actionlib::SimpleActionClient<bwi_kr_execution::ExecutePlanAction> Client;
+using namespace std;
 
 int main(int argc, char**argv) {
     ros::init(argc, argv, "leader");
@@ -13,44 +14,69 @@ int main(int argc, char**argv) {
     client.waitForServer();
 
     ros::ServiceClient client_gui = n.serviceClient<bwi_msgs::QuestionDialog>("/question_dialog");
-    
+
     while(ros::ok()){
-      bwi_msgs::QuestionDialog question;
-      question.request.type = question.request.CHOICE_QUESTION;
+        bwi_msgs::QuestionDialog question;
+        bwi_msgs::QuestionDialog userAlive;
+        question.request.type = question.request.CHOICE_QUESTION;
+        userAlive.request.type = userAlive.request.CHOICE_QUESTION;
 
-      question.request.message = "Is 439 going to steal your soul?";
-      question.request.options.push_back("yes");
-      question.request.options.push_back("no");
+        question.request.message = "Where would you like to go?";
+        question.request.options.push_back("414");
+        question.request.options.push_back("2nd door");
 
-      question.request.timeout = question.request.NO_TIMEOUT;
+        question.request.timeout = question.request.NO_TIMEOUT;
 
-      if(client_gui.call(question)){
-	if(question.response.index >= 0){
-	  switch(question.response.index){
-	  case 0:
-	    question.request.message = "Urite";
-	    break;
-	  case 1:
-	    question.request.message = "You sure about that?";
-	    break;
-	  default:
-	    ROS_ERROR("Not a valid response");
-	    return 1;
-	  }
-	}
-	else {
-	  ROS_ERROR("How did you even end up here");
-	}
-      }
-      else{
-	ROS_ERROR("Failed to call service /question_dialog");
-	return 1;
-      }
+        userAlive.request.message = "Are you still there?";
+        userAlive.request.options.push_back("yes");
+        userAlive.request.options.push_back("no");
 
-      question.request.type = question.request.DISPLAY;
-      question.request.timeout = 100000000000.000;
-      client_gui.call(question);
-      return 0;
+        if(client_gui.call(question)){
+            if(question.response.index >= 0){
+                switch(question.response.index){
+                    case 0: {
+                        question.request.message = "Going to 414";
+                        ros::NodeHandle privateNode("~");
+                        string door;
+                        privateNode.param<string>("door",door,"d3_414b1");
+                        bwi_kr_execution::ExecutePlanGoal goal;
+
+                        bwi_kr_execution::AspRule rule;
+                        bwi_kr_execution::AspFluent fluent;
+                        fluent.name = "not facing";
+
+                        fluent.variables.push_back(door);
+
+                        rule.body.push_back(fluent);
+                        goal.aspGoal.push_back(rule);
+
+                        ROS_INFO("sending goal");
+                        client.sendGoal(goal);
+
+
+                        break;
+                    }
+                    case 1:
+                        question.request.message = "You sure about that?";
+                        break;
+                    default:
+                        ROS_ERROR("Not a valid response");
+                        return 1;
+                }
+            }
+            else {
+                ROS_ERROR("How did you even end up here");
+            }
+        }
+        else{
+            ROS_ERROR("Failed to call service /question_dialog");
+            return 1;
+        }
+
+        question.request.type = question.request.DISPLAY;
+        question.request.timeout = 100000000000.000;
+        client_gui.call(question);
+        return 0;
     }
 
     return 0;
