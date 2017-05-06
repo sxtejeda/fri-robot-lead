49c0c2a4eddc3703b6c5c792dbc04acb4312bb9d
shermansxt <stonetejeda@utexas.edu>
2017-05-05 23:16:46 -0500

    movement from one location to another works fine

--- src/leader.cpp
+++ src/leader.cpp
@@ -9,7 +9,7 @@ using namespace std;
 int main(int argc, char**argv) {
   ros::init(argc, argv, "leader");
   ros::NodeHandle n;
-  
+  ros::NodeHandle privateNode("~");
   
   ros::ServiceClient client_gui = n.serviceClient<bwi_msgs::QuestionDialog>("/question_dialog");
   
@@ -25,7 +25,7 @@ int main(int argc, char**argv) {
     
     question.request.message = "Where would you like to go?";
     question.request.options.push_back("414");
-    question.request.options.push_back("2nd door");
+    question.request.options.push_back("404");
     
     question.request.timeout = question.request.NO_TIMEOUT;
     
@@ -36,13 +36,70 @@ int main(int argc, char**argv) {
     moving.request.type = moving.request.DISPLAY;
     moving.request.timeout = 0;
 
+    string door;
+
     if(client_gui.call(question)){
+      if(question.response.index >= 0){
+	switch(question.response.index){
+	  case 0:
+	    privateNode.param<string>("door",door,"d3_414b1");
+	    moving.request.message = "Going to 414";
+	    break;
+ 	  case 1:
+	    privateNode.param<string>("door",door,"d3_404");
+	    moving.request.message = "Going to 404";
+	    break;
+	  default:
+	    ROS_ERROR("Invalid input for buttons");
+	    break;
+	}
+      }
+      else {
+	ROS_ERROR("Should not be possible to get here.");
+      }
+
+      bwi_kr_execution::ExecutePlanGoal goal;
+      
+      bwi_kr_execution::AspRule rule;
+      bwi_kr_execution::AspFluent fluent;
+      fluent.name = "not facing";
+      
+      fluent.variables.push_back(door);
+      
+      rule.body.push_back(fluent);
+      goal.aspGoal.push_back(rule);
+      
+      ROS_INFO("sending goal");
+      client.sendGoal(goal);
+      
+      client_gui.call(moving);
+      ros::Rate wait_rate(10);
+      while(ros::ok() && !client.getState().isDone()){
+	wait_rate.sleep();
+      }
+      
+      if (client.getState() == actionlib::SimpleClientGoalState::ABORTED) {
+	ROS_INFO("Aborted");
+      }
+      else if (client.getState() == actionlib::SimpleClientGoalState::PREEMPTED) {
+	ROS_INFO("Preempted");
+      }
+      
+      else if (client.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
+	ROS_INFO("Succeeded!");
+      }
+      else
+	ROS_INFO("Terminated");
+      
+      
+    }
+
+    /*    if(client_gui.call(question)){
       if(question.response.index >= 0){
 	switch(question.response.index){
 	  case 0: {
 
 	    moving.request.message = "Going to 414";
-	    ros::NodeHandle privateNode("~");
 	    string door;
 	    privateNode.param<string>("door",door,"d3_414b1");
 	    bwi_kr_execution::ExecutePlanGoal goal;
@@ -57,7 +114,6 @@ int main(int argc, char**argv) {
 	    goal.aspGoal.push_back(rule);
 	  
 	    ROS_INFO("sending goal");
-	    client.cancelGoalsAtAndBeforeTime(ros::Time::now());
 	    client.sendGoal(goal);
 	  
 	    client_gui.call(moving);
@@ -91,8 +147,8 @@ int main(int argc, char**argv) {
       }
       else {
 	ROS_ERROR("How did you even end up here");
-      }
-    }
+	}
+	}*/
     else{
       ROS_ERROR("Failed to call service /question_dialog");
       return 1;
