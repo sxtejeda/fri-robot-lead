95aedac51d00a24c9b5eefa155746d25cb427aac
Luke Wright <lukewright1997@gmail.com>
2017-05-12 14:27:17 -0500

    yolo

--- fri_robot_lead/src/leader.cpp
+++ fri_robot_lead/src/leader.cpp
@@ -32,22 +32,22 @@ int main(int argc, char **argv) {
   ros::ServiceClient client_gui = n.serviceClient<lead_rqt_plugins::RoomDialog>("/room_dialog");
 
   lead_rqt_plugins::RoomDialog question;
+  question.request.type = question.request.COMBOBOX_QUESTION;
+  question.request.message = "Where would you like to go?";
 
+  for (int i = 0; i < roomCount; i++) {
+    std::string room = rooms[i];
+    question.request.options.push_back(room);
+  }
   while (ros::ok()) {
     
     lead_rqt_plugins::RoomDialog moving;
     lead_rqt_plugins::RoomDialog userAlive;
-    question.request.type = question.request.COMBOBOX_QUESTION;
     userAlive.request.type = userAlive.request.CHOICE_QUESTION;
 
     Client client("/action_executor/execute_plan", true);
     client.waitForServer();
 
-    question.request.message = "Where would you like to go?";
-    for (int i = 0; i < roomCount; i++) {
-        std::string room = rooms[i];
-        question.request.options.push_back(room);
-    }
     //question.request.options.push_back("414");
     //question.request.options.push_back("404");
 
